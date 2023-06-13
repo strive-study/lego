773133fa8cd889405e985d3630e7753853f31658
xiaoli <2585960098@qq.com>
2023-06-14 01:34:25 +0800

    feat:add InlineEdit component and test case

---
+++ src/components/InlineEdit.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="inline-edit" @click.stop="handleClick" ref="wrapper">
+    <input
+      type="text"
+      v-if="isEditing"
+      v-model="innerValue"
+      placeholder="文本不能为空"
+      ref="inputRef"
+    />
+    <slot v-else
+      ><span>{{ innerValue }}</span></slot
+    >
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const props = defineProps({
+  value: {
+    type: String,
+    required: true
+  }
+})
+const innerValue = ref(props.value)
+const isEditing = ref(false)
+const handleClick = () => {
+  isEditing.value = true
+}
+</script>
+
+<style scoped>
+.inline-edit {
+  cursor: pointer;
+}
+.input-error {
+  border: 1px solid #f5222d;
+}
+.input-error:focus {
+  border-color: #f5222d;
+}
+.input-error::placeholder {
+  color: #f5222d;
+}
+</style>
---
+++ tests/unit/InlineEdit.spec.ts
@@ -0,0 +1,40 @@
+import { VueWrapper, mount } from '@vue/test-utils'
+import InlineEdit from '@/components/InlineEdit.vue'
+import { nextTick } from 'vue'
+let wrapper: VueWrapper<any>
+const defaultProps = 'test'
+describe('InputEdit component', () => {
+  beforeAll(() => {
+    wrapper = mount(InlineEdit as any, {
+      props: {
+        value: defaultProps
+      }
+    })
+  })
+
+  it('should render the default layout', () => {
+    expect(wrapper.get('span').text()).toBe(defaultProps)
+  })
+
+  it('should render input when clicking the element', async () => {
+    await wrapper.trigger('click')
+    expect(wrapper.find('input').exists()).toBeTruthy()
+    const input = wrapper.get('input').element as HTMLInputElement
+    expect(input.value).toBe(defaultProps)
+  })
+
+  it('press enter should render to default layout with new value', async () => {
+    await wrapper.get('input').setValue('testnew')
+    const event = new KeyboardEvent('keydown', { key: 'Enter' })
+    document.dispatchEvent(event)
+    await nextTick()
+    expect(wrapper.find('span').exists()).toBeTruthy()
+    expect(wrapper.get('span').text()).toBe('testnew')
+    const events = wrapper.emitted('change')
+    expect(events[0]).toEqual(['testnew'])
+  })
+
+  it('press esc should render to default layout with old value', async () => {})
+
+  it('click outside should render to default layout with new value', async () => {})
+})

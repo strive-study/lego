13db623e26267c36663072a6c59f9872ffe83fbb
xiaoli <2585960098@qq.com>
2023-06-24 17:15:07 +0800

    chore:手动对ant-design-vue和html2canvas拆包

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 const BundleAnalyzerPlugin =
   require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const webpack = require('webpack')
 const isStaging = !!process.env.VUE_APP_STAGING
 const isProduction = process.env.NODE_ENV === 'production'
 const isAnalyzeMode = process.env.ANALYZE_MODE
@@ -28,12 +29,37 @@ module.exports = defineConfig({
     }
   },
   configureWebpack: config => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/locale$/,
+        contextRegExp: /moment$/
+      })
+    )
     if (isAnalyzeMode) {
       config.plugins.push(
         new BundleAnalyzerPlugin({
           analyzerMode: 'static'
         })
       )
+    }
+    config.optimization.splitChunks = {
+      maxInitialRequests: 30,
+      minSize: 0,
+      chunks: 'all',
+      cacheGroups: {
+        antVendor: {
+          name: 'ant-design-vue',
+          test: /[\\/]node_modules[\\/](ant-design-vue)[\\/]/
+        },
+        canvasVendor: {
+          name: 'html2canvas',
+          test: /[\\/]node_modules[\\/](html2canvas)[\\/]/
+        },
+        vendor: {
+          name: 'vendor',
+          test: /[\\/]node_modules[\\/](!html2canvas)(!ant-design-vue)[\\/]/
+        }
+      }
     }
   }
 })
